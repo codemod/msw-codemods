24cc39225cc95c25fb354d5e6220a0c1dea21c26
Amirabbas Ghasemi <amirabbas08dev@gmail.com>
2025-09-18 14:38:04 +0330

    fix: passthrough bug

--- codemods/msw-2-upgrade-recipe/scripts/req-passthrough.ts
+++ codemods/msw-2-upgrade-recipe/scripts/req-passthrough.ts
@@ -21,36 +21,85 @@ async function transform(root: SgRoot<TSX>): Promise<string> {
     },
   });
 
-  const memberExpression = rootNode.findAll({
+  let arrowFunctions = rootNode.findAll({
     rule: {
-      kind: "member_expression",
-      pattern: "$REQ.passthrough",
+      any: [
+        {
+          kind: "arrow_function",
+          pattern: "async $PARAMS => $BLOCK",
+          inside: {
+            kind: "arguments",
+            inside: {
+              kind: "call_expression",
+              has: {
+                kind: "member_expression",
+                pattern: "$MEMBER",
+              },
+            },
+          },
+        },
+        {
+          kind: "arrow_function",
+          pattern: "$PARAMS => $BLOCK",
+          inside: {
+            kind: "arguments",
+            inside: {
+              kind: "call_expression",
+              has: {
+                kind: "member_expression",
+                pattern: "$MEMBER",
+              },
+            },
+          },
+        },
+      ],
     },
   });
 
   let newImports = "";
-  if (memberExpression.length) {
-    if (hasMswImport) {
-      const importMatch = hasMswImport.getMatch("IMPORTS");
-      let importText = importMatch?.text() ?? "";
-      if (importText[0] == "{" && importText[importText.length - 1] == "}") {
-        importText =
-          importText.substring(0, importText.length - 1) + ", passthrough}";
-      } else {
-        importText += "{passthrough}";
-      }
-      edits.push(hasMswImport.replace(`import ${importText} from "msw"`));
-    } else {
-      newImports = `import { passthrough } from "msw"; \n`;
-    }
-  }
 
-  memberExpression.forEach((ex) => {
-    let req = ex.getMatch("REQ");
-    let reqText = req?.text() ?? "";
-    let exText = ex?.text() ?? "";
-    exText = exText.replace(`${reqText}.`, "");
-    edits.push(ex.replace(exText));
+  arrowFunctions.forEach((arr) => {
+    let prms = arr.getMatch("PARAMS");
+    let paramsText = prms?.text() ?? "";
+    let params = paramsText.substring(1, paramsText.length - 1);
+    let paramsArray = params.split(",") as string[];
+    if (paramsArray.length) {
+      let reqName = paramsArray[0];
+      const memberExpression = arr.findAll({
+        rule: {
+          kind: "member_expression",
+          pattern: "$REQ.passthrough",
+        },
+      });
+
+      if (memberExpression.length) {
+        if (hasMswImport) {
+          const importMatch = hasMswImport.getMatch("IMPORTS");
+          let importText = importMatch?.text() ?? "";
+          if (
+            importText[0] == "{" &&
+            importText[importText.length - 1] == "}"
+          ) {
+            importText =
+              importText.substring(0, importText.length - 1) + ", passthrough}";
+          } else {
+            importText += "{passthrough}";
+          }
+          edits.push(hasMswImport.replace(`import ${importText} from "msw"`));
+        } else {
+          newImports = `import { passthrough } from "msw"; \n`;
+        }
+      }
+
+      memberExpression.forEach((ex) => {
+        let req = ex.getMatch("REQ");
+        let reqText = req?.text() ?? "";
+        if (reqText != reqName) return;
+        let exText = ex?.text() ?? "";
+        exText = exText.replace(`${reqText}.`, "");
+        edits.push(ex.replace(exText));
+      });
+    }
   });
 
   let newSource = rootNode.text();
